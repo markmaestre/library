d7663df55995fe9f3fe3425dc0d209bdbef5603b
markmaestre <markranier.maestre@tup.edu.ph>
2025-10-19 15:46:50 +0800

    frontend deployment:

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,8 +1,26 @@
+// import axios from "axios";
+
+
+// const API = axios.create({
+//   baseURL: "http://192.168.1.44:10000", 
+// });
+
+// API.interceptors.request.use(async (config) => {
+//   const token = global.authToken;
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
+
+
 import axios from "axios";
 
-
 const API = axios.create({
-  baseURL: "http://192.168.1.44:10000", 
+  baseURL: "https://library-it.onrender.com", 
 });
 
 API.interceptors.request.use(async (config) => {
